2aee600ddcaf13c75681e4a73d057ea059415073
Javo Rosas <javier.rosasb@gmail.com>
2015-06-12 14:18:04 -0700

    add Class for cell

---
+++ Feedy/FeedCell.h
@@ -0,0 +1,20 @@
+//
+//  FeedCell.h
+//  Feedy
+//
+//  Created by Javier Rosas on 6/12/15.
+//  Copyright (c) 2015 Javier Rosas. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface FeedCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UIView *profilePicture;
+@property (weak, nonatomic) IBOutlet UIView *photo;
+@property (weak, nonatomic) IBOutlet UILabel *username;
+@property (weak, nonatomic) IBOutlet UILabel *likes;
+@property (weak, nonatomic) IBOutlet UITextView *descriptionText;
+
+@end
---
+++ Feedy/FeedCell.m
@@ -0,0 +1,15 @@
+//
+//  FeedCell.m
+//  Feedy
+//
+//  Created by Javier Rosas on 6/12/15.
+//  Copyright (c) 2015 Javier Rosas. All rights reserved.
+//
+
+#import "FeedCell.h"
+
+@implementation FeedCell
+
+
+
+@end
